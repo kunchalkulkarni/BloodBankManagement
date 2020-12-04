b2c0301ef065ff7f4c5ccb31e1ae36ab4b975247
kunchalkulkarni <kunchalkulkarni01@gmail.com>
2020-12-04 13:56:11 +0530

    Complete Project

---
+++ src/blood_bank_management_system/Blood_Bank_Management_System.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blood_bank_management_system;
+
+/**
+ *
+ * @author user
+ */
+public class Blood_Bank_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
